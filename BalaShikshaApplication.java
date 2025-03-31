bb3bf878413dad50aa7cd07cda71fe13d4bfe216
Sitanshu T <92461539+Sitanshuamcse57@users.noreply.github.com>
2025-03-31 22:48:49 +0530

    Add files via upload

---
+++ BalaShikshaApplication.java
@@ -0,0 +1,13 @@
+package com.TGEducation.BalaShiksha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalaShikshaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalaShikshaApplication.class, args);
+	}
+
+}
